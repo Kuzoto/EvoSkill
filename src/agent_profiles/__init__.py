eddb3c1929fdaddc894b610313bb0e9f2dc11f0a
salzubi401 <salaheddin@sentient.xyz>
2026-02-11 12:42:56 +0300

    added dabstep stuff

--- src/agent_profiles/__init__.py
+++ src/agent_profiles/__init__.py
@@ -1,6 +1,7 @@
 from .proposer import proposer_options
 from .skill_generator import skill_generator_options
 from .base_agent import base_agent_options, make_base_agent_options
+from .dabstep_agent import dabstep_agent_options, make_dabstep_agent_options
 from .prompt_generator import prompt_generator_options
 from .skill_proposer import skill_proposer_options
 from .prompt_proposer import prompt_proposer_options
@@ -11,6 +12,8 @@ __all__ = [
     "skill_generator_options",
     "base_agent_options",
     "make_base_agent_options",
+    "dabstep_agent_options",
+    "make_dabstep_agent_options",
     "prompt_generator_options",
     "skill_proposer_options",
     "prompt_proposer_options",
